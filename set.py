2065cf188b1fe17613fd5841fc15f62774fa64bb
Flávio Neves <contato.flavio72@gmail.com>
2021-03-16 18:48:20 -0300

    teste 1.0

---
+++ set.py
@@ -0,0 +1,108 @@
+def finder(local):
+    if local == "Conceicao":
+        return [12]
+    elif local == "Centro":
+        return [8, 9, 13]
+    elif local == "Eldorado":
+        return [10]
+    elif local == "Casa Grande":
+        return [7, 11]
+    elif local == "Taboao":
+        return [14]
+    else:
+        return []
+
+
+def define(nat, stat, local):
+    nats = ['Alagamento','Desabamento','Deslizamento','Escorregamento','Inundacao','Solapamento']
+    for nt in nats:
+        ind = nats.index(nt) + 1
+        if nat == nt:
+            testes[stat][ind]['count'] += 1
+            if local not in testes[stat][ind]['locais']:
+                testes[stat][ind]['locais'].append(local)
+
+
+def saver(name, states, names):
+    with open(name, 'w') as file:
+        file.write('ID,Alagamento,Desabamento,Deslizamento,Escorregamento,Inundacao,Solapamento,QtChuva')
+
+        for rw in states:
+            file.write('\n' + names[states.index(rw)])
+
+            for row in testes[rw].items():
+                file.write(',' + str(row[1]['count']))
+
+testes = {
+    7: {
+        1: {'count': 0, 'locais': ['']},
+        2: {'count': 0, 'locais': ['']},
+        3: {'count': 0, 'locais': ['']},
+        4: {'count': 0, 'locais': ['']},
+        5: {'count': 0, 'locais': ['']},
+        6: {'count': 0, 'locais': ['']},
+        'chu': {'count': 0, 'locais': ['']}
+    },
+    8: {1: {'count': 0, 'locais': ['']},
+        2: {'count': 0, 'locais': ['']},
+        3: {'count': 0, 'locais': ['']},
+        4: {'count': 0, 'locais': ['']},
+        5: {'count': 0, 'locais': ['']},
+        6: {'count': 0, 'locais': ['']},
+        'chu': {'count': 0, 'locais': ['']}
+    },
+    9: {
+        1: {'count': 0, 'locais': ['']},
+        2: {'count': 0, 'locais': ['']},
+        3: {'count': 0, 'locais': ['']},
+        4: {'count': 0, 'locais': ['']},
+        5: {'count': 0, 'locais': ['']},
+        6: {'count': 0, 'locais': ['']},
+        'chu': {'count': 0, 'locais': ['']}
+    },
+    10: {
+        1: {'count': 0, 'locais': ['']},
+        2: {'count': 0, 'locais': ['']},
+        3: {'count': 0, 'locais': ['']},
+        4: {'count': 0, 'locais': ['']},
+        5: {'count': 0, 'locais': ['']},
+        6: {'count': 0, 'locais': ['']},
+        'chu': {'count': 0, 'locais': ['']}
+    },
+    11: {
+        1: {'count': 0, 'locais': ['']},
+        2: {'count': 0, 'locais': ['']},
+        3: {'count': 0, 'locais': ['']},
+        4: {'count': 0, 'locais': ['']},
+        5: {'count': 0, 'locais': ['']},
+        6: {'count': 0, 'locais': ['']},
+        'chu': {'count': 0, 'locais': ['']}
+    },
+    12: {
+        1: {'count': 0, 'locais': ['']},
+        2: {'count': 0, 'locais': ['']},
+        3: {'count': 0, 'locais': ['']},
+        4: {'count': 0, 'locais': ['']},
+        5: {'count': 0, 'locais': ['']},
+        6: {'count': 0, 'locais': ['']},
+        'chu': {'count': 0, 'locais': ['']}
+    },
+    13: {
+        1: {'count': 0, 'locais': ['']},
+        2: {'count': 0, 'locais': ['']},
+        3: {'count': 0, 'locais': ['']},
+        4: {'count': 0, 'locais': ['']},
+        5: {'count': 0, 'locais': ['']},
+        6: {'count': 0, 'locais': ['']},
+        'chu': {'count': 0, 'locais': ['']}
+    },
+    14: {
+        1: {'count': 0, 'locais': ['']},
+        2: {'count': 0, 'locais': ['']},
+        3: {'count': 0, 'locais': ['']},
+        4: {'count': 0, 'locais': ['']},
+        5: {'count': 0, 'locais': ['']},
+        6: {'count': 0, 'locais': ['']},
+        'chu': {'count': 0, 'locais': ['']}
+    },
+}
