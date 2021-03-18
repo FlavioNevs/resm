e69ccdda277febe717088799bece46f2d3f27993
Flavio Neves <flavio.neevs@gmail.com>
2021-03-18 17:59:16 -0300

    correcao codec UTF

--- format.py
+++ format.py
@@ -1,12 +1,12 @@
 import csv
 import sys
 
-rr = open(sys.argv[1], "r")
-resm = csv.reader(rr)
+with open(sys.argv[1], "r", encoding="latin-1") as r:
+    resm = csv.reader(r)
 
-with open(sys.argv[2], 'w') as outro:
-    for row in resm:
-        for itens in row:
-            var = itens.replace(';', ',')
-            var = itens.replace('"', '')
+    with open(sys.argv[2], 'w') as outro:
+        for row in resm:
+            var = str(row[0])
+            var = var.replace(';', ',')
+            var = var.replace('"', '')
             outro.write(var+'\n')
