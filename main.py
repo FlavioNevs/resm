30ce158aaf8d72e6f5b78843813704aec69f5d81
Flávio Neves <favio.neevs@gmail.com>
2021-03-20 12:18:48 -0300

    help

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-
+# Importando bibliotecas
 import csv
 import sys
 from argparse import ArgumentParser
 from config.classes import Bairro, Arquivo, testes, formater, Municipio
 
+# Sistema de reconhecimento de comandos no prompt
 parser = ArgumentParser()
 subparsers = parser.add_subparsers()
 
@@ -23,6 +24,7 @@ calc.set_defaults(subparser='calc')
 
 args = parser.parse_args()
 
+# Verificação do modo de execução
 if args.subparser == 'calc':
     entrada = args.arquivo_entrada
     la = args.limiarA
@@ -30,26 +32,34 @@ if args.subparser == 'calc':
     saida = args.arquivo_saida
     mun = args.municipio
     bai = args.bairro
-    
 elif args.subparser == 'format':
     entrada = args.arquivo_entrada
     saida = args.arquivo_saida
 
-if args.subparser == 'calc' or debug:
+# verificção da execução
+if args.subparser == 'calc':
     Arq = Arquivo(entrada, saida)
+    # Se for por Bairro
     if bai:
+        # abre o arquivo data
         with open(Arq.nomeA, 'r') as f:
             file = csv.reader(f)
             mod = Bairro()
 
-            for row in file:
+            # Por linha do data
+            for row in file: 
+
                 if row[0] != mod.dias:
                     mod.locais = []
                     mod.nats = []
-            
+
+                # por estação
                 for stat in mod.states:
+
                     if row[stat] == 'NA':
                         resender = False
+
+                    # Verificação do Limiar
                     elif la < float(row[stat]) <= lb:
                         if row[0] != mod.dias:
                             testes[stat]['chu']['count'] += 1
@@ -59,6 +69,7 @@ if args.subparser == 'calc' or debug:
 
                     for result in mod.finder(row[7]):
                         ver = False
+
                         if resender and result == stat:
                             ver = mod.define(row[6], row[7], row[0])
                         elif resender:
@@ -71,16 +82,20 @@ if args.subparser == 'calc' or debug:
 
                 mod.dias = row[0]
                 mod.states = [12, 14, 16, 18, 20, 22, 24, 26]
+
             Arq.saverB(mod.states, mod.names)
+    # Se for por Município
     elif mun:
         with open(Arq.nomeA, 'r') as f:
             file = csv.reader(f)
             mod = Municipio()
 
             st = ['munmed', 'munmax']
+
             for row in file:
                 for stat in st:
                     resender = False
+
                     if row[0] != mod.dias:
                         mod.nats = []
 
@@ -89,30 +104,24 @@ if args.subparser == 'calc' or debug:
                     elif stat == 'munmax':
                         limiar = row[10]
 
-                    if la < float(limiar) <= lb:
-                        #print(f'{row[0]} | {la} > {limiar} <= {lb}')
+                    if limiar == 'NA':
+                        resender = False
+                    elif la < float(limiar) <= lb:
                         if row[0] != mod.dias:
                             testes[stat]['chu']['count'] += 1
                         resender = True
                     else:
                         resender = False
-                    
+
                     if resender:
                         if row[6] != '':
                             ver = mod.define(row[6], row[0])
-
                             if ver:
                                 mod.dine(row[6], stat)
-                                print(f'{row[0]} - {row[6]}')
                                 mod.nats.append(row[6])
-
-                    mod.dias = row[0]
-
-                
+                mod.dias = row[0]
             for stat in st:
                 Arq.saverM(la, lb, stat)
-            
-
 elif args.subparser == 'format':
     form = formater(entrada, saida)
     form.forma()
